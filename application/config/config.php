de5e7354399a7b73822671c7191f5ce312edc976
Amadou Ly <amly212782@gmail.com>
2017-04-19 08:05:50 +0200

    Cleanning up the app skeleton deletion of some files/folders

--- application/config/config.php
+++ application/config/config.php
@@ -9,7 +9,7 @@ ini_set("display_errors", 1);
 
 /**
  * Configuration for: URL
- * Here we auto-detect your applications URL and the potential sub-folder. Works perfectly on most servers and in local
+ * Here we auto-detect the applications URL and the potential sub-folder. Works perfectly on most servers and in local
  * development environments (like WAMP, MAMP, etc.). Don't touch this unless you know what you do.
  *
  * URL_PROTOCOL:
@@ -17,9 +17,6 @@ ini_set("display_errors", 1);
  *
  * URL_DOMAIN:
  * The domain. Don't change unless you know exactly what you do.
- *
- * URL_SUB_FOLDER:
- * The sub-folder. Optional, comment this out if you don't use a sub-folder.
  *
  * URL_INDEX_FILE:
  * Our index file that will be hit on every request to our application. No reason to change this in any way usually.
@@ -45,7 +42,7 @@ if (defined('URL_SUB_FOLDER')) {
 
 /**
  * Configuration for: Database
- * This is the place where you define your database credentials, database type etc.
+ * This is the place where we define our database credentials, database type etc.
  */
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
